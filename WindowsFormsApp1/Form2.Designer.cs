2cba83ef1fef4c28f4625616619b6eca5d02a89c
skifn <nanisimov143@gmail.com>
2022-06-30 17:40:36 +0300

    prufer's code c++

---
+++ WindowsFormsApp1/Form2.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 165);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(273, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "В исходном графе 4 вершины.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Принять";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Код Прюфера для графа:";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(319, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(405, 38);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Введите ребра (т.е. какая вершина к какой присоединяется)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(323, 144);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(323, 184);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(104, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(603, 144);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(118, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(467, 184);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(111, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(467, 144);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(111, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(603, 184);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(118, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(323, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 27);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Первое ребро";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(467, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 27);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Второе ребро";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(603, 114);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 27);
+            this.label7.TabIndex = 17;
+            this.label7.Text = " Третье ребро";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(234, 144);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "вер. 1";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(234, 184);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 20);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "вер. 2";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.Фон;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Кодирование дерева";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        public System.Windows.Forms.Label label5;
+        public System.Windows.Forms.Label label6;
+        public System.Windows.Forms.Label label7;
+        public System.Windows.Forms.Label label8;
+        public System.Windows.Forms.Label label9;
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+
+
+
+    class PRUFER
+    {
+
+
+        public int vertices;
+        public int[] vertex_set;
+        public int i = 0, j = 0, ver = 0, edg = 0, minimum = 0, p = 0;
+
+        public void printTreeEdges(int[] prufer, int m)
+        {
+            vertices = m + 2;
+            vertex_set = new int[vertices];
+
+            // Инициализируем массив вершин
+            for (int i = 0; i < vertices; i++)
+                vertex_set[i] = 0;
+
+            // Количество вхождений вершины в код
+            for (int i = 0; i < vertices - 2; i++)
+                vertex_set[prufer[i] - 1] += 1;
+
+            Console.Write("Дерево:\n");
+
+            // Находим наименьшую вершину, отсутствующую в
+            // коде Прюфера[]
+            int j = 0;
+            for (int i = 0; i < vertices - 2; i++)
+            {
+                for (j = 0; j < vertices; j++)
+                {
+                    // Если j + 1 отсутствует 
+                    if (vertex_set[j] == 0)
+                    {
+                        // Убираем из кода Прюфера и добавляем в 
+                        vertex_set[j] = -1;
+                        Console.Write("(" + (j + 1) + ", "
+                                      + prufer[i] + ") ");
+
+                        vertex_set[prufer[i] - 1]--;
+
+                        break;
+                    }
+                }
+            }
+
+            j = 0;
+            // Для последнего элемента
+            for (int i = 0; i < vertices; i++)
+            {
+                if (vertex_set[i] == 0 && j == 0)
+                {
+                    Console.Write("(" + (i + 1) + ", ");
+                    j++;
+                }
+                else if (vertex_set[i] == 0 && j == 1)
+                    Console.Write((i + 1) + ")\n");
+            }
+
+        }
+        public void coding()
+        {
+            Console.WriteLine("Количество вершин в графе = 4");
+            ver = 4;
+            Console.WriteLine("\n");
+            edg = ver - 1;
+
+
+
+            int[,] EDG = new int[edg, 2];
+            int[] DG = new int[ver + 1];
+
+
+            Console.WriteLine($"Это дерево имеет {edg} рёбер и {ver} вершин");
+            Console.WriteLine($"Существует {edg} пар вершин в дереве");
+
+
+
+            for (i = 0; i < edg; i++)
+            {
+                Console.WriteLine($"Введите соединение ребра {i + 1}");
+                Console.WriteLine("V(1):");
+                EDG[i, 0] = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(" V(2) :");
+                EDG[i, 1] = Convert.ToInt32(Console.ReadLine());
+
+                DG[EDG[i, 0]]++;
+                DG[EDG[i, 1]]++;
+            }
+
+
+            Console.WriteLine("Код Прюфера для дерева: ");
+            for (i = 0; i < ver - 2; i++)
+            {
+                minimum = 10000;
+                for (j = 0; j < edg; j++)
+                {
+                    if (DG[EDG[j, 0]] == 1)
+                    {
+                        if (minimum > EDG[j, 0])
+                        {
+                            minimum = EDG[j, 0];
+                            p = j;
+                        }
+                    }
+                    if (DG[EDG[j, 1]] == 1)
+                    {
+                        if (minimum > EDG[j, 1])
+                        {
+                            minimum = EDG[j, 1];
+                            p = j;
+                        }
+                    }
+                }
+                DG[EDG[p, 0]]--; // удаляем вершину уменьшением ее степени до нуля
+                DG[EDG[p, 1]]--; // уменьшим степень другой вершины
+
+                if (DG[EDG[p, 0]] == 0)
+                    Console.WriteLine($"{EDG[p, 1]}");
+                else
+                    Console.WriteLine($"{EDG[p, 0]}");
+            }
+
+        }
+
+    }
+
+    class Vertex
+    {
+        public int  vertices;
+
+        Vertex() { }
+
+        ~Vertex() { }
+    }
+
+
+    class Edge
+    {
+
+        public int v1, v2;
+
+        Edge() { }
+
+        ~Edge() { }
+    }
+
+
+}
+
